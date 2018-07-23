5494cb63a09d38cea4ad42fbb4ffecb1f78aa230
yuanming.lee91 <yuanming.lee91@gmail.com>
2018-07-23 00:35:35 -0400

    Refactoring forms into partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
   def new
     @article = Article.new
   end
@@ -36,7 +38,17 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully destroyed"
+    redirect_to articles_path
+  end
+  
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
     def article_params
       params.require(:article).permit(:title, :description)
     end
